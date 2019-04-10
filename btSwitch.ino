73673291900c2b561a3c640d9f0b52c47fb18667
jbaxter25 <baxterjo@oregonstate.edu>
2019-04-09 17:40:22 -0700

    Initial commit

---
+++ btSwitch.ino
@@ -0,0 +1,32 @@
+/************************************
+ * File Name: btSwitch.ino
+ * Author: Jordan Baxter
+ * Group: Blue (1)
+ * Group Members: Jordan Baxter, Suyang Liu, Trenton Kilgore
+ * 
+ * Description: This is the 'main' file for the bluetooth controlled
+ * power outlet. Made for ECE 342 in Spring 2019.
+ */
+
+#include <string.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+#if SOFTWARE_SERIAL_AVAILABLE
+  #include <SoftwareSerial.h>
+#endif
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
