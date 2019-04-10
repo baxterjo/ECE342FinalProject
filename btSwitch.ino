3b9687f180ab9359e1ccbbfcd4a2818066a16c01
jbaxter25 <baxterjo@oregonstate.edu>
2019-04-09 17:50:36 -0700

    Started setup drawing codebase from controller example from adafruit.

--- btSwitch.ino
+++ btSwitch.ino
@@ -21,6 +21,26 @@
   #include <SoftwareSerial.h>
 #endif
 
+#define FACTORYRESET_ENABLE         1
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+    #define MODE_LED_BEHAVIOUR          "MODE"
+
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+// function prototypes over in packetparser.cpp
+uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
+float parsefloat(uint8_t *buffer);
+void printHex(const uint8_t * data, const uint32_t numBytes);
+
+// the packet buffer
+extern uint8_t packetbuffer[];
 void setup() {
   // put your setup code here, to run once:
 
